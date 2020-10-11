2e46d9d661a59b068a1924d784ccb88f9ee55c79
Elijah Nunez <elijahnunez728@gmail.com>
2020-10-10 20:49:00 -0700

    "Added a new message"

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Leonardo");
 	System.out.println("Hello Leonardo, my name is Tiago");
+	System.out.println("Hello Leonardo, my name is Elijah");
     }
 }
