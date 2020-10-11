05c3fcda2792969237f688fb95698ff52bd1e8b2
trebuhcsogait <schubert.tiago@gmail.com>
2020-10-10 23:41:04 -0400

    Hello from Tiago

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Leonardo");
+	System.out.println("Hello Leonardo, my name is Tiago");
     }
 }
